5681e39c6e10ee4ce96f330c1f2bb5063f3b2bfd
himanshu010 <himanshuaswal@yahoo.com>
2020-08-07 22:08:54 +0530

    initial commit

---
+++ public/js/info.js
@@ -0,0 +1,80 @@
+const queryString = window.location.search;
+console.log(queryString);
+// ?fname=johnny&lname=depp
+
+const urlParams = new URLSearchParams(queryString);
+
+const weathd = document.querySelector("#weathD");
+const temp = document.querySelector("#temp");
+const winds = document.querySelector("#winds");
+const press = document.querySelector("#pressure");
+const precipitation = document.querySelector("#precip");
+const humid = document.querySelector("#humidity");
+const fil = document.querySelector("#fil");
+const uv = document.querySelector("#uv");
+const visib = document.querySelector("#visib");
+const address = document.querySelector("#location");
+const icon = document.getElementById("wicon");
+let theme = document.getElementById("theme");
+
+address.textContent = "Loading...";
+weathd.textContent = "Loading...";
+temp.textContent = "Loading...";
+winds.textContent = "Loading...";
+press.textContent = "Loading...";
+precipitation.textContent = "Loading...";
+humid.textContent = "Loading...";
+fil.textContent = "Loading...";
+uv.textContent = "Loading...";
+visib.textContent = "Loading...";
+
+const location_name = urlParams.get("location");
+console.log(location_name);
+
+const url = "http://localhost:3000/weather?address=" + location_name;
+fetch(url).then((response) => {
+  response.json().then((whole_data) => {
+    if (whole_data.error) {
+      console.log(whole_data.error);
+    } else {
+      const {
+        weatherDescription,
+        temperature,
+        icon_url,
+        windSpeed,
+        pressure,
+        precip,
+        humidity,
+        feelslike,
+        uv_index,
+        visibility,
+        isDay,
+      } = whole_data.data;
+
+      console.log(whole_data.data);
+
+      const location = whole_data.location;
+
+      address.textContent = location;
+      weathd.textContent = weatherDescription;
+      temp.textContent = temperature;
+      winds.textContent = windSpeed;
+      press.innerHTML = pressure;
+      precipitation.innerHTML = precip;
+      humid.textContent = humidity;
+      fil.textContent = feelslike;
+      uv.textContent = uv_index;
+      visib.textContent = visibility;
+      var img = document.createElement("img");
+      img.src = icon_url;
+      icon.appendChild(img);
+      img.style.borderRadius = "5px";
+
+      if (isDay === "yes") {
+        theme.href = "css/styleinfoday.css";
+      } else {
+        theme.href = "css/styleinfonight.css";
+      }
+    }
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const express = require("express");
+console.log(__dirname);
+console.log(path.join(__dirname, ".."));
+const hbs = require("hbs");
+
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast.js");
+
+const app = express();
+
+//defining paths for handlebars
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handlebars
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+//seting up static files. So that
+//we don't have to give location
+//of whole file present in public folder
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "weather app 1",
+    name: "himanshu aswal",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "about me",
+    about: "about123",
+    color: "red",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    help: "help12343",
+    title: "help",
+    name: "himanshu",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "you must provide a address",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, data) => {
+        if (error) {
+          return res.send({ error });
+        }
+        res.send({ data, location });
+      });
+    }
+  );
+});
+
+app.get("/weatherinfo", (req, res) => {
+  if (!req.query.location) {
+    return res.send("must provide an address");
+  }
+  res.render("weather-info", {
+    location: req.query.location,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.send("My 404 page");
+});
+
+app.listen(3000, () => {
+  console.log("server is up on port 3000");
+});
